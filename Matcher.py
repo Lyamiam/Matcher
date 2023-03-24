bd4754e5d7b957a62934d0acfea4a43210ea2c69
Lyamiam <lyam.temple2@gmail.com>
2023-03-24 06:24:02 +0000

    added basic docker image file

--- Matcher.py
+++ Matcher.py
@@ -1,11 +1,13 @@
 #!/bin/python
 
 from flask import Flask
+from flask import render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route("/")
+def titleScreen():
     return "Welcome to Matcher"
 
-app.run(host="0.0.0.0", port=80)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
